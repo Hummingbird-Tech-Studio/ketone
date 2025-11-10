3f98ff4e0558bc8fdc301a91bc85089ece15fb32
Aperez <andres.perezcorona81@gmail.com>
2025-11-10 11:05:36 -0600

    feat: add sign-up route with SignUpView component in router configuration

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SignUpView from '@/views/signUp/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/sign-up',
+      name: 'signup',
+      component: () => SignUpView,
+    },
+  ],
 })
 
 export default router
